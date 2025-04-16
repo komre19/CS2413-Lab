8fd5c14bcee99d2c7e5b84abad47d942c9ce84e7
Renard Kombo <148933075+komre19@users.noreply.github.com>
2025-04-15 23:42:17 -0500

    Update Lab_3.c

--- Lab_3.c
+++ Lab_3.c
@@ -1,8 +1,10 @@
 // Renard Kombo
 // R#: 11786591
 
-// Lab 3 part 1
+// Lab 3 
 // 4/15/2025
+
+// PART 1 
 
 #include <stdlib.h>
 
@@ -21,6 +23,7 @@ void preorder(struct TreeNode* root, int* result, int* returnSize) {
     preorder(root->right, result, returnSize);
 }
 
+// main funct.
 int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     *returnSize = 0;
     // allocate for max 100 nodes 
@@ -30,4 +33,33 @@ int* preorderTraversal(struct TreeNode* root, int* returnSize) {
 }
 
 //////////////////////////////////////////////////////////////////////
+//                    Run separately                                //
+//////////////////////////////////////////////////////////////////////
 
+// PART 2
+
+#include <stdlib.h>
+
+// binary tree node
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// recursive funct.
+void inorder(struct TreeNode* root, int* result, int* returnSize) {
+    if (root == NULL) return;
+    inorder(root->left, result, returnSize);
+    result[(*returnSize)++] = root->val;
+    inorder(root->right, result, returnSize);
+}
+
+// main function
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    *returnSize = 0;
+    // allocate for max 100 nodes
+    int* result = (int*)malloc(100 * sizeof(int)); 
+    inorder(root, result, returnSize);
+    return result;
+}
